30d00872426960b3ddd3f5a1b316d612cd22f358
Ramos <marcuram@amazon.com>
2017-11-15 07:51:53 +0200

    First commit, with SAM template, lambda function and readme

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# lambda-ec2-deregiter-old-amis
+# Lambda EC2 Deregister Old AMIs
+
 A serverless solution to deregister old AMIs created as backup using "lambda-ec2-instance-backup" which creates automated AMIs.
+
+## Getting Started
+
+This project contains two files:
+
+CloudFormation SAM template (template.yaml)
+
+Lambda Function (index.py)
+
+The Lambda function will deregister Amazon Machine Images (AMI) from your AMIs which contains tag "backup" set as "lambda-ec2-instance-backup", as created by the serverless solution [lambda-ec2-instance-backup](https://github.com/mvinii94/lambda-ec2-instance-backup), each hour.
+
+### Prerequisites
+
+* **S3 Bucket** - *To upload the index.py file zipped.* 
+
+* **AWS CLI Installed** - *To deploy the SAM CloudFormation template.* - [How to Install AWS CLI](http://docs.aws.amazon.com/cli/latest/userguide/installing.html)
+
+### Deploying the lambda serverless application
+
+* Clone this repository;
+
+* Upload the lambda code zipped to an S3 Bucket of your preference in the desired region (should be the same where you are going to deploy the serverless solution);
+
+* Run the following command providing the bucket name and the S3 key name toof your file:
+
+```
+aws cloudformation deploy --template-file template.yaml --stack-name lambda-ec2-deregister-old-amis --region us-east-1 --capabilities CAPABILITY_IAM --parameter-overrides Bucket=<MyBucketName> FileKey=lambda-ec2-deregister-old-amis.zip RetentionPeriod=0.1
+```
+
+## Authors
+
+* **Marcus Ramos** - [GitHub](https://github.com/mvinii94/)
---
+++ index.py
@@ -0,0 +1,52 @@
+import boto3
+import logging
+from datetime import datetime, timedelta
+
+
+ec2 = boto3.client('ec2')
+amis = ec2.describe_images(Filters=[ {'Name': 'tag:backup', 'Values': ['lambda-ec2-instance-backup']} ])
+snapshots = ec2.describe_snapshots(MaxResults=1000)['Snapshots']
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+ami_created_in={}
+
+def lambda_handler(event, context):
+    #PRINT THE PARAMETERS
+    logger.info('Parameters received: {0}'.format(event))
+    #GET THE DAYS PARAMETER TO USE AS RETENTION
+    days = event['retention']
+    #CALCULATE THE TIMESTAMP TO COMPARE TO CHECK IF THE AMI BACKUP IS OLD ENOUGH
+    days_ago = datetime.now() - timedelta(days=int(days))
+    logger.info('Timestamp with {0} day(s) ago to compare : {1}'.format(days,days_ago))
+    #CREATE AMI_TAGS DICTIONARY
+    ami_tags = {}
+    #SEARCH AMI'S AND CHECK IF IT IS OLD ENOUGH IF IT IS, DELETES IT.
+    for ami in amis['Images']:
+        ami_tags.update( { ami['ImageId'] : tag['Value'] for tag in ami['Tags'] if tag['Key'] == 'CreationDate' } )
+        #PRINT AMI_TAGS
+        ami_created_in = ami_tags[ami['ImageId']]
+
+        # CONVERT THE CREATION TIME TAG (STRING) TO DATETIME
+        date_object = datetime.strptime(ami_created_in, "%Y-%m-%d %H:%M:%S.%f")
+        logger.info('-----------------------------------------------------------')
+        logger.info('AMI: ' + ami['ImageId'] + ' CREATED IN: ' + ami_created_in)
+
+        if date_object < days_ago:
+            logger.info('DEREGISTERING AMI: ' + ami['ImageId'])
+            try:
+                amiResponse = ec2.deregister_image(
+                    DryRun=False,
+                    ImageId=ami['ImageId'],
+                )
+                for snapshot in snapshots:
+                    if snapshot['Description'].find(ami['ImageId']) > 0:
+                        snap = ec2.delete_snapshot(SnapshotId=snapshot['SnapshotId'])
+                        logger.info('-----------------------------------------------------------')
+                        logger.info("DELETING SNAPSHOT: " + snapshot['SnapshotId'])
+                        logger.info('-----------------------------------------------------------')
+            except:
+                logger.info('Something went wrong, please check the logs for more information')
+        else:
+            logger.info("BACKUP NOT OLD ENOUGH.")
+        logger.info('-----------------------------------------------------------')
